b457362d929074daa3b9c908b61e6bb3a4bb010a
wintertia <amoes.noland@gmail.com>
2025-09-24 09:52:14 +0700

    fix: hmr actually works

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,12 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
+    server: {
+      hmr: true,
+      watch: {
+        usePolling: true
+      }
+    }
   }
 });
