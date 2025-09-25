6e089b980b348906d43d6f3e4cf42705b72668aa
wintertia <amoes.noland@gmail.com>
2025-09-25 22:48:40 +0700

    feat: add husky to run prettier before commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
-import icon from 'astro-icon';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,10 +10,10 @@ export default defineConfig({
     server: {
       hmr: true,
       watch: {
-        usePolling: true
-      }
-    }
+        usePolling: true,
+      },
+    },
   },
 
-  integrations: [icon()]
+  integrations: [icon()],
 });
